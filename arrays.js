e5240eebf666bb527d6a0db2c081d68c04c07769
troyma2007 <146706129+troyma2007@users.noreply.github.com>
2023-11-07 23:02:19 +0100

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let myarray = [1, 2, 3, 4, 5, 6, 7, 8,];
+console.log(myarray)
+console.log(myarray[0] + myarray[1] + myarray[2] );
+
+for(let i = 0; i< myarray.length; i++){
+
+    console.log(myarray[i]);
+
+}
+
+ 
+
+ 
